aa0813d87c16f941559f7abcf2f34c98edfd04b9
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-03-29 08:29:30 +0530

    printing numbers by using loops

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,25 @@
-import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int marks = sc.nextInt();
-        if (marks>=35){
-            if (marks>=90){
-                System.out.println("Grade A");
-            }
-            else if (marks>=85){
-                System.out.println("Grade B");
-            }
-            else{
-                System.out.println("Grade C");
-            }
+        System.out.println("----FOR LOOP----");
+        for(int i=1;i<=5;i++){
+            System.out.println(i+" ");
         }
-        else{
-            System.out.println("Fail");
+
+        System.out.println("----WHILE LOOP----");
+        int j=1;
+        while(j<=5){
+            System.out.println(j+" ");
+            j++;
         }
+
+        System.out.println("----DO-WHILE LOOP----");
+        int k=1;
+        do { 
+            System.out.println(k+" ");
+            k++;
+        } while (k<=5);
     }
+    
 }
