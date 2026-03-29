b66f173d3083d894bd6842b144e417b5de27bd02
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-03-29 08:11:29 +0530

    alarm by using switch case

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Student {
     public static void main(String[] args) {
-        System.out.println("Hii");
+        Scanner sc = new Scanner(System.in);
+        String day = sc.nextLine();
+        switch(day){
+            case "saturday","sunday":
+                System.out.println("Alarm at 6AM");
+                break;
+            case "monday":
+                System.out.println("Alarm at 7AM");
+                break;
+            default:
+                System.out.println("Alarm at 8AM");
+        }
     }
 }
