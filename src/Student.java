646d854f17b3e339ade277e122056b1b4f7ff418
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-03-29 10:40:41 +0530

    message from parents

--- src/Student.java
+++ src/Student.java
@@ -1,22 +1,13 @@
-
 class Studs{
-    String name;
-    int id;
-    void display(){
-        System.out.println(name+" : "+id);
+    private int marks=28;
+    public void msg(){
+        System.out.println("study well");
     }
 }
-
 public class Student{
     public static void main(String[] args){
         Studs s1 = new Studs();
-        Studs s2 = new Studs();
-        s1.name= "PinkCat";
-        s1.id=246;
-        s2.name="PinkDog";
-        s2.id=183;
-        s1.display();
-        s2.display();
-
+        System.out.println("Message form parents");
+        s1.msg();
     }
 }
