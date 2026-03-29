e32d158066f6b7fad58123061d9ddc3e321f20ec
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-03-29 10:29:17 +0530

    class,object

--- src/Student.java
+++ src/Student.java
@@ -1,17 +1,22 @@
-import java.util.Scanner;
-public class Student {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String day = sc.nextLine();
-        switch(day){
-            case "saturday","sunday":
-                System.out.println("Alarm at 6AM");
-                break;
-            case "monday":
-                System.out.println("Alarm at 7AM");
-                break;
-            default:
-                System.out.println("Alarm at 8AM");
-        }
+
+class Studs{
+    String name;
+    int id;
+    void display(){
+        System.out.println(name+" : "+id);
+    }
+}
+
+public class Student{
+    public static void main(String[] args){
+        Studs s1 = new Studs();
+        Studs s2 = new Studs();
+        s1.name= "PinkCat";
+        s1.id=246;
+        s2.name="PinkDog";
+        s2.id=183;
+        s1.display();
+        s2.display();
+
     }
 }
