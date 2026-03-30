07930aa9607a462d35ea1761f69fc88800290531
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-03-30 20:41:24 +0530

    car

--- src/Demo.java
+++ src/Demo.java
@@ -1,19 +1,15 @@
-class Laptop {
-    String model;
-    int price;
-    String color;   
-    public String toString(){
-        return "model: "+model+" price: "+price+" color: "+color;
+class Car{
+    public void drive(){
+        System.out.println("Car is driving");
+    }
+    public void playMusic(){
+        System.out.println("Playing music in the car");
     }
 }
-
-class Demo{
-    public static void main(String[] args) {
-        Laptop obj = new Laptop();
-        obj.model = "Lenovo";
-        obj.price = 150000;
-        obj.color = "Grey";
-        System.out.println(obj);
-        
+public class Demo{
+    public static void main(String[] args){
+        Car obj = new Car();
+        obj.drive();
+        obj.playMusic();
     }
 }
