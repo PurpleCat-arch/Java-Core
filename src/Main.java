16f70480403e9f8c1d202b288867a4c073d5e434
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-03-30 19:03:09 +0530

    first non repeating character

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,14 @@
+// first non repeating character in a string
 public class Main{
     public static void main(String[] args){
-        int arr[] ={2,3,4,5,6,7,8};
-        int max=arr[0];
-        int min=arr[0];
-        // first find maximum and minimum
-        for (int i=0; i<arr.length;i++){
-            if (arr[i]>max){
-                max = arr[i];
+        String name = "Alice";
+        for (int i=0; i<name.length(); i++){
+            char c= name.charAt(i);
+            if (name.indexOf(c) == name.lastIndexOf(c)){
+                System.out.println("First non repeating character is: " + c);
+                break;
             }
-            if (arr[i]<min){
-                min = arr[i];
-            }
+            
         }
-        System.out.println("First Maximum: " + max);
-        System.out.println("First Minimum: " + min);
-
-        int secondmax=arr[0];
-        int secondmin=arr[0];
-
-        // second find maximum and minimum
-        for (int i=0; i<arr.length;i++){
-            if (arr[i]>secondmax && arr[i]<max){
-                secondmax = arr[i];
-            }
-            if (arr[i]<secondmin && arr[i]>min){
-                secondmin = arr[i];
-            }
-        }
-        System.out.println("Second Maximum: " + secondmax);
-        System.out.println("Second Minimum: " + secondmin);
     }
 }
