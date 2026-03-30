a3a3670a8f062d66c9b8989d967ba0bce5d3eb5f
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-03-30 20:16:54 +0530

    Calculator

--- src/Demo.java
+++ src/Demo.java
@@ -1,27 +1,31 @@
-// import java.util.Scanner;
-class Human{
-    private int age;
-    private String name;
-    public int getAge(){
-        return age;
+class Calc{
+    public int add(int a,int b){
+        return a+b;
     }
-    String getName(){
-        return name;
-    }
-    public void setAge(int age){
-        this.age = age;
-    }
-    public void setName(String name){
-        this.name = name;
+    public int sub(int a,int b){
+        return a-b;
     }
 }
-
-public class Demo {
-    public static void main(String[] args) {
-        Human obj = new Human();
-        obj.setAge(20);
-        obj.setName("purple cat");
-        System.out.println("Name: " + obj.getName());
-        System.out.println("Age: " + obj.getAge());
+class AdvCalc extends Calc{
+    public int multiply(int a,int b){
+        return a*b;
+    }
+    public int divide(int a,int b){
+        return a/b;
+    }
+}
+class SuperCalc extends AdvCalc{
+    public int power(int a,int b){
+        return (int) Math.pow(a,b);
+    }
+}
+public class Demo{
+    public static void main(String[] args){
+        SuperCalc sc = new SuperCalc();
+        System.out.println("Addition: "+sc.add(10,5));
+        System.out.println("Subtraction: "+sc.sub(10,5));
+        System.out.println("Multiplication: "+sc.multiply(10,5));
+        System.out.println("Division: "+sc.divide(10,5));
+        System.out.println("Power: "+sc.power(2,3));    
     }
 }
