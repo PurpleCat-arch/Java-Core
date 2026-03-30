701c0fe150c3692e0fcc25c60894c786ff7a97d6
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-03-30 17:44:43 +0530

    second maximum and minimum

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,33 @@
-class Main {
-    public int[] twoSum(int[] nums,int target){
-        for(int i=0;i<nums.length;i++){
-            for (int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i, j};
-                }
+public class Main{
+    public static void main(String[] args){
+        int arr[] ={2,3,4,5,6,7,8};
+        int max=arr[0];
+        int min=arr[0];
+        // first find maximum and minimum
+        for (int i=0; i<arr.length;i++){
+            if (arr[i]>max){
+                max = arr[i];
+            }
+            if (arr[i]<min){
+                min = arr[i];
             }
         }
-        return new int[]{};
-    }
-    public static void main(String[] args){
-        Main main = new Main();
-        int[] nums = new int[]{3,4,5,6,7,8,9};
-        int target =9;
-        int[] result = main.twoSum(nums, target);
-        System.out.println("[" + result[0] + ", " + result[1]+"]");
+        System.out.println("First Maximum: " + max);
+        System.out.println("First Minimum: " + min);
 
+        int secondmax=arr[0];
+        int secondmin=arr[0];
+
+        // second find maximum and minimum
+        for (int i=0; i<arr.length;i++){
+            if (arr[i]>secondmax && arr[i]<max){
+                secondmax = arr[i];
+            }
+            if (arr[i]<secondmin && arr[i]>min){
+                secondmin = arr[i];
+            }
+        }
+        System.out.println("Second Maximum: " + secondmax);
+        System.out.println("Second Minimum: " + secondmin);
     }
 }
