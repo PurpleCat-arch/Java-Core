fcbb9b9843a836f18282ab626ea84deafb4b50cf
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-03-31 20:31:56 +0530

    try catch

--- src/Demo.java
+++ src/Demo.java
@@ -1,20 +1,10 @@
-class Student {
-    int id;
-    String name;
-
-    Student(int i, String n) {
-        id = i;
-        name = n;
-    }
-
-    void display() {
-        System.out.println("ID: " + id + ", Name: " + name);
-    }
-}
-
-public class Demo {
-    public static void main(String[] args) {
-        Student s1 = new Student(101, "purple cat");
-        s1.display();
+public class Demo{
+    public static void main(String[] args){
+        try {
+            @SuppressWarnings("unused")
+            int data  = 100 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed.");
+        }
     }
 }
