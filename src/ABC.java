7bce601bfde00f8e6b7969de9c307efddddaf2d8
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-03-31 22:32:18 +0530

    custom exception

---
+++ src/ABC.java
@@ -0,0 +1,16 @@
+class MyException extends Exception {
+    public MyException(String message) {
+        super(message);
+    }   
+}
+public class ABC {
+    public static void main(String[] args){
+        try {
+            throw new MyException("This is a custom exception");
+        } 
+        catch (MyException e) {
+            System.out.println("Caught exception ");
+            System.out.println( e.getMessage());
+        }
+    }
+}
