f485a0289a155a0b35f6677c668bd16264656f7f
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-03-31 21:19:08 +0530

    finally block

--- src/Demo.java
+++ src/Demo.java
@@ -1,17 +1,16 @@
 public class Demo {
     public static void main(String[] args) {
         try {
-            int a[] = new int[5]; 
-            a[5] = 30 / 0;
+            int data =10/0;
+            System.out.println(data);
         }
         catch (ArithmeticException e) {
             System.out.println("ArithmeticException");
         }
-        catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("ArrayIndexOutOfBoundsException");
+        finally {
+            System.out.println("finally block is always executed");
         }
-         catch (Exception e) {
-            System.out.println("Exception");
-        }
+        System.out.println("rest of the code...");
+        
     }
 }
