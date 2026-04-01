8a7c92ced283028a31f89a544ec1ffb640f9d6f9
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-01 21:18:14 +0530

    Area

--- src/ABC.java
+++ src/ABC.java
@@ -1,16 +1,26 @@
-class MyException extends Exception {
-    public MyException(String message) {
-        super(message);
-    }   
+class Shape{
+    void area(){
+        System.out.println("area is not defined");
+    }
+    void area(int side){
+        System.out.println("area oa square is "+ side*side);
+    }
+    void area(int length, int breadth){
+        System.out.println("area of rectangle is "+ length*breadth);
+    }
 }
-public class ABC {
+class Circle extends Shape{
+    void area(int radius){
+        System.out.println("area of circle is "+ 3.14*radius*radius);
+    }
+}
+public class ABC{
     public static void main(String[] args){
-        try {
-            throw new MyException("This is a custom exception");
-        } 
-        catch (MyException e) {
-            System.out.println("Caught exception ");
-            System.out.println( e.getMessage());
-        }
+        Shape s = new Shape();
+        s.area();
+        s.area(5);
+        s.area(4, 6);
+        Shape c = new Circle();
+        c.area(3);
     }
 }
