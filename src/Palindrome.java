f7c6b0cc53f33f4b0f6994e7d8c94cee0a832986
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-01 08:03:17 +0530

    Palindrome for String

---
+++ src/Palindrome.java
@@ -0,0 +1,18 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String str = "madam";
+        String rev = "";
+
+        // reverse string
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        // check
+        if (str.equals(rev)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
