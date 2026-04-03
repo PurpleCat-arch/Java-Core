d60c66a263f20c4a133c5b05c1ac47583d22ccd6
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-03 17:27:07 +0530

    array 2

---
+++ src/Array2.java
@@ -0,0 +1,14 @@
+public class Array2 {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+
+        int sum=0;
+        for (int num:arr){
+            sum = sum+num;
+        }
+
+        double average = (double)sum/arr.length;
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+    }
+}
