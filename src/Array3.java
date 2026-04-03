539795d23834e6fca6556d31dbb118ab541dcced
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-03 17:32:38 +0530

    array 3

---
+++ src/Array3.java
@@ -0,0 +1,19 @@
+public class Array3 {
+    public static void main(String[] args) {
+        int[] arr = {5,7,8,3,2,1};
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int num: arr) {
+            if (num>max) {
+                max=num;
+            }
+            if (num<min) {
+                min= num;
+            }
+        }
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+    }
+}
