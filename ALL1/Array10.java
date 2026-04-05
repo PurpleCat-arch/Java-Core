43cb52414ddbc44557ee6ec22e2e34d8dbb4d99d
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-05 21:29:26 +0530

    count occurrences of target element

---
+++ ALL1/Array10.java
@@ -0,0 +1,15 @@
+public class Array10 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 2, 4};
+        int target = 2;
+
+        int count = 0;
+
+        for (int num : arr) {
+            if (num == target)
+                count++;
+        }
+
+        System.out.println("Occurrences = " + count);
+    }
+}
