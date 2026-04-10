ce08d2f698b296aa9e6a88891719c8e89891fb28
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-10 18:48:00 +0530

    Static method examples

---
+++ src/Static2.java
@@ -0,0 +1,9 @@
+public class Static2 {
+    static void show() {
+        System.out.println("Static method called");
+    }
+
+    public static void main(String[] args) {
+        Static2.show();
+    }
+}
