e90004b4788e6c877f44d2d5afa4a059e0ab055c
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-10 19:08:47 +0530

    Final Class Example

---
+++ src/Final2.java
@@ -0,0 +1,12 @@
+final class A {
+    void display() {
+        System.out.println("Final class");
+    }
+}
+public class Final2 {
+     public static void main(String[] args) {
+        A obj = new A();
+        obj.display();
+    }
+
+}
