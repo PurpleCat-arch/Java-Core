b852ca2e16626caa74bd31d6c2d9f6e2d2159c9c
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-10 18:58:10 +0530

    Final Variable Example

---
+++ src/Final1.java
@@ -0,0 +1,9 @@
+public class Final1 {
+    final int x=10;
+
+    public static void main(String[] args) {
+        Final1 obj = new Final1();
+        System.out.println("Value of x: " + obj.x);
+
+    }
+}
