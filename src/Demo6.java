3a6a3e460e62910b2b2a3692533b676997a7249d
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-12 09:49:26 +0530

    Runtime Polymorphism

---
+++ src/Demo6.java
@@ -0,0 +1,17 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal sound");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+public class Demo6 {
+    public static void main(String[] args) {
+        Animal a = new Dog(); // polymorphism
+        a.sound();
+    }
+}
