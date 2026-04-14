e3aaf3af232516c51c71713300515ac34a4aef12
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-14 10:54:32 +0530

    add constructor

--- src/Student3.java
+++ src/Student3.java
@@ -6,7 +6,14 @@ class Studs3{
     int marks1;
     int marks2;
 
-
+    Studs3(int id, String name, int age, int totmarks, int marks1, int marks2){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.totmarks=totmarks;
+        this.marks1=marks1;
+        this.marks2=marks2;
+    }
 }
 public class Student3 {
     public static void main(String[] args) {
