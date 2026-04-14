30d065c23554b336b9a605fd64532a8048bbcae8
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-14 10:51:09 +0530

    mini project(create student class)

---
+++ src/Student3.java
@@ -0,0 +1,14 @@
+class Studs3{
+    int id;
+    String name;
+    int age;
+    int totmarks;
+    int marks1;
+    int marks2;
+
+
+}
+public class Student3 {
+    public static void main(String[] args) {
+    }
+}
