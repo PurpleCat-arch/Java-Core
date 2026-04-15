ba338a54dde696f661bd5f525b2849a8d1fc2c91
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-15 12:10:35 +0530

    Add Constructor

--- src/Account.java
+++ src/Account.java
@@ -2,6 +2,13 @@ public class Account {
     private int accNo;
     private String name;
     private double balance;
+
+    Account(int accNo, String name, double balance) {
+    this.accNo = accNo;
+    this.name = name;
+    this.balance = balance;
+    }
+    
     public static void main(String[] args) {
         
     }
