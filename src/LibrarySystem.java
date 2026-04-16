6c75b9f70762c9dbfc2880e044b1ce4130364cb9
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-16 17:18:58 +0530

    add constructor

--- src/LibrarySystem.java
+++ src/LibrarySystem.java
@@ -2,6 +2,12 @@ class Book{
     int id;
     String name;
     boolean isIssued;
+
+    Book(int id, String name) {
+    this.id = id;
+    this.name = name;
+    this.isIssued = false;
+    }
 }
 public class LibrarySystem {
     public static void main(String[] args){
