cfc1b7eedd9762eaf418bcc3ae7d938d3d4afb55
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-17 20:08:26 +0530

    Add Product to Cart

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -33,7 +33,43 @@ public class ShoppingCart {
 
     static Product[] cart = new Product[100];
     static int count = 0;
-    
+
+    static void addProduct(java.util.Scanner sc) {
+    System.out.print("Enter Product ID: ");
+    int id = sc.nextInt();
+    sc.nextLine();
+
+    System.out.print("Enter Product Name: ");
+    String name = sc.nextLine();
+
+    System.out.print("Enter Price: ");
+    double price = sc.nextDouble();
+
+    if (price <= 0) {
+        System.out.println("Invalid price!");
+        return;
+    }
+
+    cart[count++] = new Product(id, name, price);
+    System.out.println("Product added to cart!");
+    }
+
+    static void removeProduct(java.util.Scanner sc) {
+    System.out.print("Enter Product ID to remove: ");
+    int id = sc.nextInt();
+
+    for (int i = 0; i < count; i++) {
+        if (cart[i].id == id) {
+            for (int j = i; j < count - 1; j++) {
+                cart[j] = cart[j + 1];
+            }
+            count--;
+            System.out.println("Product removed!");
+            return;
+        }
+    }
+    System.out.println("Product not found!");
+    }
     public static void main(String[] args){
 
     }
