2bb6037f92f90cddfa0de52f3a7565027a4d6e1f
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-21 20:54:09 +0530

    add constructor

--- src/QuizSystem.java
+++ src/QuizSystem.java
@@ -2,6 +2,13 @@ class Question{
     String question;
     String[] options;
     int correctAnswer;
+
+    Question(String queston, String[] options,int correctAnswers){
+        this.question=question;
+        this.options=options;
+        this.correctAnswer=correctAnswer;
+        
+    }
 }
 public class QuizSystem{
     public static void main(String[] args){
