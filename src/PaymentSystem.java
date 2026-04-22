303757d2b3d1c5bef52ec432770c1aa621555af3
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-22 22:14:52 +0530

    create abstract class

--- src/PaymentSystem.java
+++ src/PaymentSystem.java
@@ -2,8 +2,21 @@ interface Payment {
     void pay(double amount);
 }
 
+abstract class Account {
+    int accNo;
+    String name;
+
+    Account (int accNo,String name){
+        this.accNo=accNo;
+        this.name=name;
+    }
+
+    abstract void display();
+}
+
 public class PaymentSystem {
     public static void main(String[] args){
 
     }
 }
+
