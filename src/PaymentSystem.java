6c13f7af9da86df5a9250486b3165e7d7c31734d
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-23 22:13:53 +0530

    Add User Method

--- src/PaymentSystem.java
+++ src/PaymentSystem.java
@@ -48,6 +48,26 @@ class Wallet implements Payment {
 public class PaymentSystem {
     static User[] users = new User[50];
     static int count=0;
+
+    static void addUser(java.util.Scanner sc) {
+    System.out.print("Enter Account No: ");
+    int accNo = sc.nextInt();
+    sc.nextLine();
+
+    System.out.print("Enter Name: ");
+    String name = sc.nextLine();
+
+    if (name.isEmpty()) {
+        System.out.println("Invalid name!");
+        return;
+    }
+
+    users[count++] = new User(accNo, name);
+    System.out.println("User added!");
+    }
+
+    
+
     public static void main(String[] args){
 
     }
