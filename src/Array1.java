d831436fd0893db89d128618b03eefba5ebb7500
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-24 22:08:40 +0530

    updated with new values

--- src/Array1.java
+++ src/Array1.java
@@ -1,7 +1,7 @@
 
 public class Array1 {
     public static void main(String[] args){
-        int[] arr={1,2,3,4,5};
+        int[] arr={4,5,6,7,8};
 
         for(int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
