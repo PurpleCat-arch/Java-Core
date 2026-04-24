f7023a795d6636c5decfbb45e87188405b344759
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-24 22:19:30 +0530

    updated with new values

--- src/Array7.java
+++ src/Array7.java
@@ -1,6 +1,6 @@
 public class Array7 {
     public static void main(String[] args){
-        int[] arr ={1,2,3,4,4,3,3};
+        int[] arr ={1,2,3,4,4,3,3,8,7,9,3,8,6,4,2,3,1,3,4,6,9};
         int duplicate =0;
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] == arr[i+1]) {
