d206438e78f41be5175c33ef22e898df743d28c2
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-24 22:15:40 +0530

    updated with new values

--- src/Array5.java
+++ src/Array5.java
@@ -3,7 +3,7 @@ public class Array5 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = {10,20,30,40,50};
+        int[] arr = {10,20,30,40,50,60,70,80,90};
 
         System.out.println("Enter the Element: ");
         int key = sc.nextInt();
