6d13ee75cfbe665a1f249dc040a505c527239c45
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-25 22:34:42 +0530

    Create an ArrayList and add elements

---
+++ src/ArrayListFundamentals.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ArrayListFundamentals {
+    public static void main(String[] args) {
+        
+        // 1. Create an ArrayList and add elements
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        fruits.add("Orange");
+
+        System.out.println("Initial List: " + fruits);
+
+
+    }
+}
+
