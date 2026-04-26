6786d28616e90a09a2bc0c6b8d207a531fd97248
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-26 22:20:41 +0530

    Store name in arraylist

---
+++ src/ArrayListProblems.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ArrayListProblems {
+    public static void main(String[] args){
+        // 1. Store student names in ArrayList
+        ArrayList<String> students = new ArrayList<>();
+        students.add("");
+        students.add("Anjali");
+        students.add("Rahul");
+        students.add("Vikram");
+
+        System.out.println("Student List: " + students);
+    }
+}
