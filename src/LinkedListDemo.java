b205edb27c0d5757e415cbd88496e78c920e9a2e
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-04-27 12:21:29 +0530

    Create LinkedList and add elements

---
+++ src/LinkedListDemo.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+
+        // 1. Create LinkedList and add elements
+        LinkedList<String> list = new LinkedList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        System.out.println("Initial List: " + list);
+
+
+
+       
+
+        
+
+       
+    }
+}
