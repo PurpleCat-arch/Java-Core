2bb593974684ac61c26871a1fcb73b6ab6c3f58b
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-05-02 19:56:46 +0530

    feat: add implementations for core array algorithms and problems

---
+++ 02-Arrays/KadanesAlgorithm.java
@@ -0,0 +1,25 @@
+public class KadanesAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+        
+        System.out.println("Maximum contiguous sum is " + maxSubArraySum(arr));
+    }
+
+    public static int maxSubArraySum(int[] arr) {
+        int maxSoFar = Integer.MIN_VALUE;
+        int maxEndingHere = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            maxEndingHere = maxEndingHere + arr[i];
+            
+            if (maxSoFar < maxEndingHere) {
+                maxSoFar = maxEndingHere;
+            }
+            
+            if (maxEndingHere < 0) {
+                maxEndingHere = 0;
+            }
+        }
+        return maxSoFar;
+    }
+}
