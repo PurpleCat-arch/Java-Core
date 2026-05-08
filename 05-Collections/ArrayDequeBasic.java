b27c77b27e6c2e4c785055260b266e7a5257350c
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-05-08 18:42:27 +0530

    feat: add comprehensive examples and demonstrations of core Java Collections Framework classes and utility methods

--- 05-Collections/ArrayDequeBasic.java
+++ 05-Collections/ArrayDequeBasic.java
@@ -1,1 +1,14 @@
-import java.util.ArrayDeque;\n\npublic class ArrayDequeBasic {\n    public static void main(String[] args) {\n        ArrayDeque<String> deque = new ArrayDeque<>();\n        deque.add(\"Middle\");\n        deque.addFirst(\"Front\");\n        deque.addLast(\"Back\");\n        \n        System.out.println(\"ArrayDeque: \" + deque);\n        deque.removeFirst();\n        System.out.println(\"After removeFirst: \" + deque);\n    }\n}\n
+import java.util.ArrayDeque;
+
+public class ArrayDequeBasic {
+    public static void main(String[] args) {
+        ArrayDeque<String> deque = new ArrayDeque<>();
+        deque.add("Middle");
+        deque.addFirst("Front");
+        deque.addLast("Back");
+        
+        System.out.println("ArrayDeque: " + deque);
+        deque.removeFirst();
+        System.out.println("After removeFirst: " + deque);
+    }
+}
