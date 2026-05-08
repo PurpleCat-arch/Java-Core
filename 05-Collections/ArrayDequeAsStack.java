54a1ce2e057ba8f12e3d96f7c26ac85889e57c77
PurpleCat-arch <abigpurplecat246@gmail.com>
2026-05-08 18:42:10 +0530

    feat: implement comprehensive Java Collections Framework demonstration examples

--- 05-Collections/ArrayDequeAsStack.java
+++ 05-Collections/ArrayDequeAsStack.java
@@ -1,1 +1,14 @@
-import java.util.ArrayDeque;\n\npublic class ArrayDequeAsStack {\n    public static void main(String[] args) {\n        ArrayDeque<Integer> stack = new ArrayDeque<>();\n        stack.push(1);\n        stack.push(2);\n        stack.push(3);\n        \n        System.out.println(\"Stack (using ArrayDeque): \" + stack);\n        System.out.println(\"Popped: \" + stack.pop());\n        System.out.println(\"After pop: \" + stack);\n    }\n}\n
+import java.util.ArrayDeque;
+
+public class ArrayDequeAsStack {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        
+        System.out.println("Stack (using ArrayDeque): " + stack);
+        System.out.println("Popped: " + stack.pop());
+        System.out.println("After pop: " + stack);
+    }
+}
